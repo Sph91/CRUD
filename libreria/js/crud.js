659adf3a74c4ca0a513b3eca753ab72ced0760b6
Sph91~ <sepuente.dwfsc10@ucamp.utel.edu.mx>
2022-09-22 00:54:57 -0500

    modifique ind. css js

---
+++ libreria/js/crud.js
@@ -0,0 +1,29 @@
+var nuevoId;
+var db=openDatabase("itemDB","1.0","itemDB", 65535)
+
+function limpiar () {
+    document.getElementById("item"). value="";
+    document.getElementById(importancia). value="";
+}
+
+// Funcionalidad de los botones //
+
+$ (function () { 
+$ ("#crear") .click(function() {
+    db.transaction(function(transaction) {
+        var sql="CREATE TABLE Idea"+
+        "(id INTERGER NOT NULL PRIMARY KEY AUTOINCREMENT, "+
+        "item VARCHAR(100) NOT NULL, "+
+        "Importancia ENTERO NOT NULL)";
+        transaction.executeSql(squl,undefined, function() {
+            alert("Tabla creada exitosamente") ;
+        
+        }, function ( transaction, err ) {
+                alert(err.message);
+    
+        })
+        }); 
+
+    });
+
+})
